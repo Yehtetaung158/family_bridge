e2e170c1b43eefb5235ca66f79f848f462bea2b3
Yehtetaung158 <yha15380@gmail.com>
2026-07-01 20:02:44 +0630

    day 02

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ import transactionRouter from './routes/transaction';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// app.use(cors());
 app.use(cors());
+
 app.use(express.json());
 
 app.use('/health', healthRouter);
